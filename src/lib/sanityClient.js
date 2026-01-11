e8c5d6ea87e33d3ea4af275f68b066669bd223e8
Buska <57123390+Sebuska29190@users.noreply.github.com>
2026-01-11 19:45:12 +0100

    Update sanityClient.js

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,8 +1,9 @@
+// src/lib/sanityClient.js
 import { createClient } from '@sanity/client'
 
-export const sanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // musi być import.meta.env
-  dataset: import.meta.env.VITE_SANITY_DATASET,      // musi być import.meta.env
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: '2023-10-01',
   useCdn: true,
 })
